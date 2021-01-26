5e8cb9479d2a46d92a0e61c108e973f5766e4ecf
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-01-26 12:58:42 -0400

    Second Day of Alura immersion (states and linter)

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React from 'react';
+import styled from 'styled-components';
+
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const Title = styled.h1`
 //   font-size: 50px;
 //   color: ${({ theme }) => theme.colors.primary};
-//`
-const BackgroundImage = styled.div `
+// `
+// eslint-disable-next-line no-unused-vars
+const BackgroundImage = styled.div`
   background-image: url(${db.bg});
   flex: 1;
   background-size: cover;
   background-position: center;
-`
+`;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -29,8 +34,29 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - RLSP</title>
+        <meta name="title" content="AluraQuiz - RLSP" />
+        <meta name="description" content="" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nextjs-rlsp-ac4jxlwsd.vercel.app/" />
+        <meta property="og:title" content="AluraQuiz - RLSP" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://nextjs-rlsp-ac4jxlwsd.vercel.app/" />
+        <meta property="twitter:title" content="QAluraQuiz - RLSP" />
+        <meta property="twitter:description" content="" />
+        <meta property="twitter:image" content="" />
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -38,7 +64,28 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function onSubmit(event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // eslint-disable-next-line no-console
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function input(event) {
+                  // eslint-disable-next-line no-console
+                  console.log(event.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(event.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length < 2}>
+                Play the quiz
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
